9c448430c28ba80dbec2839897c971721a76d285
Alexey Pavlyukov <alexey.pavlyukov@gmail.com>
2019-10-26 01:37:10 +0300

    add step 9

--- realworld/httpservice/service.go
+++ realworld/httpservice/service.go
@@ -2,7 +2,9 @@ package httpservice
 
 import (
 	"example.com/realworld/stor"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // Service represents the HTTP service
@@ -14,8 +16,30 @@ type Service struct {
 func (s *Service) SetupAPI(e *echo.Echo) error {
 	api := e.Group("/api")
 	api.GET("/articles", s.ArticleList)
+	api.GET("/articles/feed", s.ArticleFeed, s.restricted)
+
+	api.Use()
 
 	api.POST("/users", s.UserCreate)
 	api.POST("/users/login", s.UserLogin)
 	return nil
+}
+
+func (s *Service) restricted(next echo.HandlerFunc) echo.HandlerFunc {
+	jwtM := middleware.JWTWithConfig(middleware.JWTConfig{
+		Skipper:       middleware.DefaultSkipper,
+		SigningMethod: middleware.AlgorithmHS256,
+		ContextKey:    "user",
+		Claims:        jwt.MapClaims{},
+		SigningKey:    []byte(jwtSecret),
+		TokenLookup:   "header:Authorization",
+		AuthScheme:    "Token",
+	})
+	return jwtM(func(c echo.Context) error {
+		user := c.Get("user").(*jwt.Token)
+		claims := user.Claims.(jwt.MapClaims)
+		id := claims["id"].(float64)
+		c.Set("userID", uint(id))
+		return next(c)
+	})
 }
